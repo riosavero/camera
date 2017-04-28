dc139d638f03c4bafa5cc3bbe1cb723921c9d6af
riosavero <riosaveroo@gmail.com>
2017-04-28 17:38:27 +0700

    camera

--- app/src/main/java/com/project/art/digital/android/myapplication5/Main2Activity.java
+++ app/src/main/java/com/project/art/digital/android/myapplication5/Main2Activity.java
@@ -24,7 +24,7 @@ public class Main2Activity extends AppCompatActivity {
             setContentView(R.layout.activity_main2);
         }
 
-        public void onClickLogOut(View v) {
+        public void onClickSignOut (View v) {
             Intent submit = new Intent(Main2Activity.this, MainActivity.class);
             startActivity(submit);
         }
